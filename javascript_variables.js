5d3119b69eb2e1ea6812d076cfa65b9fcd29bdc3
Sergeb250 <142254236+Sergeb250@users.noreply.github.com>
2025-05-11 01:55:03 -1100

    Create javascript_variables.js

---
+++ javascript_variables.js
@@ -0,0 +1,81 @@
+//exercise1
+
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+//exercise2
+
+let meal = 'Enchiladas';
+console.log(meal); // Output: Enchiladas
+meal = 'Burrito';
+console.log(meal); // Output: Burrito
+
+
+//exercise 3
+
+let price;
+console.log(price); // Output: undefined
+price = 350;
+console.log(price); // Output: 350
+
+
+//exercise 4
+
+let changeMe = true;
+
+changeMe = false;
+
+console.log(changeMe);
+
+//exercise 5
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+
+levelUp += 5
+powerLevel -= 100
+multiplyMe *= 11
+quarterMe /= 4
+
+
+
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+//exercise 6
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++
+lostDollar--
+
+console.log(gainedDollar);
+console.log(lostDollar);
+
+//exercise 7
+let Myname=serge;
+ Myname=2;
+
+console.log(typeof Myname)
+
+// exercise 8
+
+let Myname = serge;
+
+let myCity=Kigali;
+
+console.log(`my name is ${Myname}. City is ${myCity}`)
+
