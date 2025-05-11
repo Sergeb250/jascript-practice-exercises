4080fac29dd2ad69025b6452b381b2098eec2271
Sergeb250 <142254236+Sergeb250@users.noreply.github.com>
2025-05-11 02:54:04 -1100

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,18 @@
+//Declaring variable of myAge
+let myAge=50
+let earlyYears=2
+//years calculation
+earlyYears*= 10.5
+let laterYears=myAge-=2
+laterYears*=4
+
+let myAgeInDogYears=earlyYears+laterYears
+
+let myName="SERGE".toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+)
+
+
+
+
+
